43915c9e5852fe094a4c3df8a2443851e90247fa
Joey Cos <joeycosentino24@gmail.com>
2022-03-18 18:35:05 -0400

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// when start button is clicked, user is presented with a question (not alerts, but on the page)
+
+// when the button is clicked, also a timer starts (this might go with an add event listener)
+
+// after each question is answered, automitcally another question is presented until the user either runs out of time or questions
+
+// when a question is answered incorrectly, time is subtracted from the clock
+
+// when all questions are answered or time equals zero, quiz is done
+
+// when the game is over, the user can save their score and initia

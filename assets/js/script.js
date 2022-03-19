0571ae9b5383c41b14df7ad3a4af6a1c50b030f8
Joey Cos <joeycosentino24@gmail.com>
2022-03-18 20:28:12 -0400

    began js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,25 @@
 
 // when all questions are answered or time equals zero, quiz is done
 
-// when the game is over, the user can save their score and initia
+// when the game is over, the user can save their score and initials
+
+
+
+// first step: what happens when button is clicked? quiz appears in form of html, so html will have to be dynamically created.
+
+// therefore an event listener should be added to the button, and we need to get the button with query selector
+
+var generateQuiz = document.querySelector("#start-quiz");
+var timerStart = document.querySelector("#timer");
+
+timerStart = setInterval(60000);
+
+// now once button is cicked, you must run a function to create the html
+
+var generateQuestion = function() {
+
+}
+
+// we also need to make timer start counting when button is clicked. link both?
+
+generateQuiz.addEventListener("submit", generateQuestion);

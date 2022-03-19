6b082d0aadb509286cf6471592cbe093ca78066c
Joey Cos <joeycosentino24@gmail.com>
2022-03-19 18:44:52 -0400

    starting playing with getting questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,25 +16,82 @@
 
 // therefore an event listener should be added to the button, and we need to get the button with query selector
 
-var generateQuiz = document.querySelector("#start-quiz");
+var buttonEl = document.querySelector("#start-quiz");
 var timerStart = document.querySelector("#timer");
+var secondsLeft = 15;
+
+var questions = [
+{
+    question: "Commonly used data types do NOT include:",
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctChoice: "Alerts"
+},
+{
+    question: "The condition in an if/else statement is enclosed in:",
+    choices: ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],
+    correctChoice: "Parenthesis"
+},
+{
+    question: "Arrays in JavaScript can be used to store:",
+    choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+    correctChoice: "All of the above"
+},
+{
+    question: "String values must be enclosed within ______ when being assigned to variables",
+    choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+    correctChoice: "Quotes"
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "Terminal Bash", "for loops", "console.log"],
+    correctChoice: "console.log"
+}
+];
+console.log(questions)
+
 
 // now once button is cicked, you must run a function to create the html
-var sec = 15;
-var time = setInterval(myTimer, 1000);
+
+function start(){
+    myTimer()
+    //disable the start button
+    buttonEl.innerHTML = selected; disabled;
+    //write a function that its going to show you the first question
+    var getQuestion = function() {
+        if(secondsLeft > 0) {
+            document.querySelector("#quiz-q").textContent = questions[0].question;
+            // show the choices
+            var displayChoices = function() {
+            document.querySelector("#quiz-a").textContent = questions[0].choices;
+            };
+        }
+    };
+
+    // show the choices
+    var displayChoices = function() {
+        document.querySelector("#quiz-a").textContent = questions[0].choices;
+    };
+}
+
+function startQuiz(){
+    //show the question
+    //add event listeners to the button answrs
+    //write a function in those event listeners that evaluate the answer and then go to next question either way
+}
+
 
 function myTimer() {
-    // why does below work as getElement by id but not as query selector
-    timerStart.innerHTML = sec + " sec left";
-    sec--;
-    if (sec == -1) {
+    var time = setInterval(function(){
+        timerStart.innerHTML = secondsLeft + " sec left";
+    secondsLeft--;
+    if (secondsLeft == -1) {
         clearInterval(time);
         alert("Time out!");
     }
+
+    }, 1000);
 }
 
 // we also need to make timer start counting when button is clicked. link both?
 
-generateQuiz.addEventListener("click", myTimer);
-
-generateQuiz
+buttonEl.addEventListener("click", start);
